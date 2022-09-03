5f622e4b3e78d62c2bda59c0f6b115339df14cfc
Gavin <gavinporlich@gmail.com>
2022-09-03 00:11:04 -0500

    Seeds added for department, employee_role, and employee

--- db/schema.sql
+++ db/schema.sql
@@ -3,30 +3,30 @@ CREATE DATABASE IF NOT EXISTS company_db;
 
 USE company_db;
 
-CREATE TABLE department(
-    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY
+CREATE TABLE department (
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
     department_name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE employee_role(
-    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY
-    role_title VARCHAR(30) NOT NULL
-    role_salary DECIMAL NOT NULL
-    department_id INTEGER NOT NULL
+CREATE TABLE employee_role (
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    role_title VARCHAR(30) NOT NULL,
+    role_salary DECIMAL NOT NULL,
+    department_id INTEGER NOT NULL,
     FOREIGN KEY(department_id)
     REFERENCES department(id)
     ON DELETE CASCADE
 );
 
-CREATE TABLE employee(
-    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY
-    first_name VARCHAR(30) NOT NULL
-    last_name VARCHAR(30) NOT NULL
-    role_id INTEGER NOT NULL
-    manager_id INTEGER
+CREATE TABLE employee (
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER,
     FOREIGN KEY(role_id)
     REFERENCES employee_role(id)
-    ON DELETE CASCADE
+    ON DELETE CASCADE,
     FOREIGN KEY(manager_id)
     REFERENCES employee(id)
     ON DELETE SET NULL
---
+++ db/seeds.sql
@@ -0,0 +1,32 @@
+DELETE FROM department;
+DELETE FROM employee_role;
+DELETE FROM employee;
+
+INSERT INTO department (department_name)
+VALUES
+    ( "Sales" ),
+    ( "Engineering" ),
+    ( "Finance" ),
+    ( "Legal" );
+
+INSERT INTO employee_role (role_title, role_salary, department_id)
+VALUES
+    ( "Salesperson", 80000, 1 ),
+    ( "Sales Lead", 110000, 1 ),
+    ( "Software Engineer", 120000, 2 ),
+    ( "Lead Engineer", 150000, 2 ),
+    ( "Accountant", 125000, 3 ),
+    ( "Account Manager", 160000, 3 ),
+    ( "Lawyer", 190000, 4 ),
+    ( "Legal Team Lead", 250000, 4 );
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+    ( "Wil", "Villarreal", 2, NULL ),
+    ( "Gracie", "Blake", 1, 1 ),
+    ( "Eliza", "Winter", 4, NULL ),
+    ( "Fergus", "Povey", 3, 3 ),
+    ( "Eliza", "Winter", 6, NULL ),
+    ( "Niall", "Davies", 5, 5 ),
+    ( "Zhane", "Vargas", 8, NULL ),
+    ( "Connagh", "Phelps", 7, 7 );
