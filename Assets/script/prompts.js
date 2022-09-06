667a13a93ef7a598fc67ba9e2ac7aeae7d179eeb
Gavin <gavinporlich@gmail.com>
2022-09-06 02:23:36 -0500

    Change employee role function finished

--- Assets/script/prompts.js
+++ Assets/script/prompts.js
@@ -18,10 +18,10 @@ function loadCLI() {
     async function mainMenuPrompt() {
         let mainPrompt = await inquirer
         .prompt({
-        type: "list",
-        name: "mainSel",
-        message: "What would you like to do?",
-        choices: ["view all departments", "view all roles", "view all employees", "add a department", "add a role", "add an employee", "update employee role", "exit"]
+            type: "list",
+            name: "mainSel",
+            message: "What would you like to do?",
+            choices: ["view all departments", "view all roles", "view all employees", "add a department", "add a role", "add an employee", "update employee role", "exit"]
         })
 
         if (mainPrompt.mainSel === "view all departments") {
@@ -59,8 +59,11 @@ function viewDepartments() {
 
 function viewRoles() {
     db.query(`SELECT employee_role.role_title AS role,
-        employee_role.role_salary AS salary
-        FROM employee_role`, function (err, results) {
+        employee_role.role_salary AS salary,
+        department.department_name as department
+        FROM employee_role
+        INNER JOIN department
+        ON employee_role.department_id = department.id`, function (err, results) {
         if (err) {
         console.log(err);
         };
@@ -77,8 +80,7 @@ function viewEmployees() {
         employees.manager_id AS manager
         FROM employees
         INNER JOIN employee_role
-        ON employees.role_id = employee_role.id
-       `, function (err, results) {
+        ON employees.role_id = employee_role.id`, function (err, results) {
         if (err) {
         console.log(err);
         } else {
@@ -89,23 +91,55 @@ function viewEmployees() {
 };
 
 function addDepartment() {
-    db.query(``, function (err, results) {
-        if (err) {
-        console.log(err);
-        };
-        console.table(results);
-        loadCLI();
-    });
+    async function newDepartment() {
+        const newDepartmentPrompt = await inquirer
+        .prompt({
+            type: "input",
+            name: "deptName",
+            message: "What is the name of the new department?"
+        })
+        db.query(`INSERT INTO department (department_name)
+            VALUES ( "${newDepartmentPrompt.deptName}" )`, function (err, results) {
+            if (err) {
+            console.log(err);
+            };
+            viewDepartments();
+        });
+    };
+    newDepartment();
 };
 
 function addRole() {
-    db.query(``, function (err, results) {
-        if (err) {
-        console.log(err);
-        };
-        console.table(results);
-        loadCLI();
-    });
+    async function newRole() {
+        const newRolePrompt = await inquirer
+        .prompt([
+            {
+            type: "input",
+            name: "roleName",
+            message: "What is the title of the new role?"
+            },
+            {
+            type: "input",
+            name: "roleSalary",
+            message: "What is the salary of the new role?"
+            },
+            {
+            type: "input",
+            name: "roleDepartment",
+            message: "What is the id of the department associated with that role?"
+            },
+
+        ])
+        db.query(`INSERT INTO employee_role (role_title, role_salary, department_id)
+            VALUES 
+                ( "${newRolePrompt.roleName}", ${newRolePrompt.roleSalary}, ${newRolePrompt.roleDepartment} ) `, function (err, results) {
+            if (err) {
+            console.log(err);
+            };
+            viewRoles();
+        });
+    };
+    newRole();
 };
 
 function addEmployee() {
@@ -119,13 +153,88 @@ function addEmployee() {
 };
 
 function updateEmployeeRole() {
-    db.query(``, function (err, results) {
-        if (err) {
-        console.log(err);
-        };
-        console.table(results);
-        loadCLI();
-    });
+    const empArr = [];
+    const roleArrName = [];
+    let roleIndex;
+    function setEmpArr() {
+        db.query(`SELECT employees.first_name AS first,
+        employees.last_name AS last
+        FROM employees`, 
+        function (err, results) {
+            if (err) {
+                console.log(err);
+            };
+            results.forEach(el => {
+                let currentEmp = "";
+                currentEmp += el.first + " " + el.last;
+                empArr.push(currentEmp);
+            });
+            setRoleArr();
+        });
+    };
+
+    function setRoleArr() {
+        let currentName;
+        db.query(`SELECT employee_role.role_title AS role
+        FROM employee_role`, 
+        function (err, results) {
+            if (err) {
+                console.log(err);
+            };
+            results.forEach(el => {
+                currentName = el.role;
+                roleArrName.push(currentName);
+            });
+            changeRole();
+        });
+    };
+
+    async function changeRole() {
+        let changeRolePrompt = await inquirer
+        .prompt([
+            {
+            type: "list",
+            name: "empSel",
+            message: "Which employee are you updating?",
+            choices: empArr
+            },
+            {
+            type: "list",
+            name: "roleSel",
+            message: "Which role are they becoming?",
+            choices: roleArrName
+            }
+        ]);
+        getIndex(changeRolePrompt);
+    };
+
+    function runUpdateQuery(changeRolePrompt) {
+        const nameStr = changeRolePrompt.empSel
+        const nameArr = nameStr.split(" ");
+        console.log(roleIndex);
+        db.query(`UPDATE employees
+        SET role_id = ?
+        WHERE employees.first_name = ? AND employees.last_name = ?`, [roleIndex, nameArr[0], nameArr[1]], function (err, results) {
+            if (err) {
+            console.log(err);
+            };
+            loadCLI();
+        });
+    };
+
+    function getIndex(changeRolePrompt) {
+        db.query(`SELECT employee_role.id AS roleid
+        FROM employee_role
+        WHERE role_title = ?`, changeRolePrompt.roleSel, function (err, results) {
+            if (err) {
+            console.log(err);
+            };
+            roleIndex = results[0].roleid;
+            runUpdateQuery(changeRolePrompt);
+        });
+    };
+
+    setEmpArr();
 };
 
 module.exports = { loadCLI, viewDepartments, viewRoles, viewEmployees, addDepartment, addRole, addEmployee, updateEmployeeRole };
