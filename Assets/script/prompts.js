8e6a95a7b1ad2ce31eabd5345b9ed221f1db9f3d
Gavin <gavinporlich@gmail.com>
2022-09-07 14:17:04 -0500

    Refactoring/Update employee role has error with getIndex function

--- Assets/script/prompts.js
+++ Assets/script/prompts.js
@@ -15,6 +15,9 @@ const db = mysql.createConnection(
   );
 
 function loadCLI() {
+    const empArr = [];
+    const roleArrName = [];
+    
     async function mainMenuPrompt() {
         let mainPrompt = await inquirer
         .prompt({
@@ -41,153 +44,207 @@ function loadCLI() {
         } else {
         process.exit();
         };
-};
+    };
 
-    mainMenuPrompt();
-};
-
-function viewDepartments() {
-    db.query(`SELECT department.department_name AS department
-        FROM department`, function (err, results) {
-        if (err) {
-        console.log(err);
-        };
-        console.table(results);
-        loadCLI();
-    });
-};
-
-function viewRoles() {
-    db.query(`SELECT employee_role.role_title AS role,
-        employee_role.role_salary AS salary,
-        department.department_name as department
-        FROM employee_role
-        INNER JOIN department
-        ON employee_role.department_id = department.id`, function (err, results) {
-        if (err) {
-        console.log(err);
-        };
-        console.table(results);
-        loadCLI();
-    });
-};
-
-function viewEmployees() {
-    db.query(`SELECT employees.first_name AS first,
-        employees.last_name AS last,
-        employee_role.role_title AS department,
-        employee_role.role_salary AS salary,
-        em.first_name AS manager
-        FROM employees
-        INNER JOIN employee_role
-        ON employees.role_id = employee_role.id
-        LEFT JOIN employees em
-        ON employees.manager_id = em.id`, function (err, results) {
-        if (err) {
-        console.log(err);
-        } else {
+    function viewDepartments() {
+        db.query(`SELECT department.department_name AS department
+            FROM department`, function (err, results) {
+            if (err) {
+            console.log(err);
+            };
             console.table(results);
             loadCLI();
-        };
-    });
-    // db.query(`WITH RECURSIVE Emp_CTE (id, first, last, manager, manager_name)
-    // AS (
-    //     SELECT employees.id AS id,
-    //     employees.first_name AS first, 
-    //     employees.last_name AS last,
-    //     employees.manager_id AS manager,
-    //     cast(NULL as CHAR(30))
-    //     FROM employees
-    //     WHERE employees.manager_id IS NULL
-    //     UNION ALL
-    //         SELECT e.id, e.first_name, e.last_name , e.manager_id, Emp_CTE.first
-    //         FROM employees e
-    //         INNER JOIN Emp_CTE
-    //         ON Emp_CTE.id = e.manager_id
-    //     )
-    // SELECT first,
-    // last,
-    // employee_role.role_title AS role,
-    // employee_role.role_salary AS salary,
-    // manager_name
-    // FROM Emp_CTE
-    // INNER JOIN employee_role
-    // ON Emp_CTE.id = employee_role.id`, function (err, results) {
-    // if (err) {
-    // console.log(err);
-    // } else {
-    //     console.table(results);
-    //     loadCLI();
-    // };
-    // });
-};
+        });
+    };
 
-function addDepartment() {
-    async function newDepartment() {
-        const newDepartmentPrompt = await inquirer
-        .prompt({
-            type: "input",
-            name: "deptName",
-            message: "What is the name of the new department?"
-        })
-        db.query(`INSERT INTO department (department_name)
-            VALUES ( "${newDepartmentPrompt.deptName}" )`, function (err, results) {
+    function viewRoles() {
+        db.query(`SELECT employee_role.role_title AS role,
+            employee_role.role_salary AS salary,
+            department.department_name as department
+            FROM employee_role
+            INNER JOIN department
+            ON employee_role.department_id = department.id`, function (err, results) {
             if (err) {
             console.log(err);
             };
-            viewDepartments();
+            console.table(results);
+            loadCLI();
         });
     };
-    newDepartment();
-};
 
-function addRole() {
-    async function newRole() {
-        const newRolePrompt = await inquirer
-        .prompt([
-            {
-            type: "input",
-            name: "roleName",
-            message: "What is the title of the new role?"
-            },
-            {
-            type: "input",
-            name: "roleSalary",
-            message: "What is the salary of the new role?"
-            },
-            {
-            type: "input",
-            name: "roleDepartment",
-            message: "What is the id of the department associated with that role?"
-            },
+    function viewEmployees() {
+        db.query(`SELECT employees.first_name AS first,
+            employees.last_name AS last,
+            employee_role.role_title AS department,
+            employee_role.role_salary AS salary,
+            em.first_name AS manager
+            FROM employees
+            INNER JOIN employee_role
+            ON employees.role_id = employee_role.id
+            LEFT JOIN employees em
+            ON employees.manager_id = em.id`, function (err, results) {
+            if (err) {
+            console.log(err);
+            } else {
+                console.table(results);
+                loadCLI();
+            };
+        });
+        // db.query(`WITH RECURSIVE Emp_CTE (id, first, last, manager, manager_name)
+        // AS (
+        //     SELECT employees.id AS id,
+        //     employees.first_name AS first, 
+        //     employees.last_name AS last,
+        //     employees.manager_id AS manager,
+        //     cast(NULL as CHAR(30))
+        //     FROM employees
+        //     WHERE employees.manager_id IS NULL
+        //     UNION ALL
+        //         SELECT e.id, e.first_name, e.last_name , e.manager_id, Emp_CTE.first
+        //         FROM employees e
+        //         INNER JOIN Emp_CTE
+        //         ON Emp_CTE.id = e.manager_id
+        //     )
+        // SELECT first,
+        // last,
+        // employee_role.role_title AS role,
+        // employee_role.role_salary AS salary,
+        // manager_name
+        // FROM Emp_CTE
+        // INNER JOIN employee_role
+        // ON Emp_CTE.id = employee_role.id`, function (err, results) {
+        // if (err) {
+        // console.log(err);
+        // } else {
+        //     console.table(results);
+        //     loadCLI();
+        // };
+        // });
+    };
 
-        ])
-        db.query(`INSERT INTO employee_role (role_title, role_salary, department_id)
-            VALUES 
-                ( "${newRolePrompt.roleName}", ${newRolePrompt.roleSalary}, ${newRolePrompt.roleDepartment} ) `, function (err, results) {
+    function addDepartment() {
+        async function newDepartment() {
+            const newDepartmentPrompt = await inquirer
+            .prompt({
+                type: "input",
+                name: "deptName",
+                message: "What is the name of the new department?"
+            })
+            db.query(`INSERT INTO department (department_name)
+                VALUES ( "${newDepartmentPrompt.deptName}" )`, function (err, results) {
+                if (err) {
+                console.log(err);
+                };
+                viewDepartments();
+            });
+        };
+        newDepartment();
+    };
+
+    function addRole() {
+        async function newRole() {
+            const newRolePrompt = await inquirer
+            .prompt([
+                {
+                type: "input",
+                name: "roleName",
+                message: "What is the title of the new role?"
+                },
+                {
+                type: "input",
+                name: "roleSalary",
+                message: "What is the salary of the new role?"
+                },
+                {
+                type: "input",
+                name: "roleDepartment",
+                message: "What is the id of the department associated with that role?"
+                },
+
+            ]);
+            db.query(`INSERT INTO employee_role (role_title, role_salary, department_id)
+                VALUES 
+                    ( "${newRolePrompt.roleName}", ${newRolePrompt.roleSalary}, ${newRolePrompt.roleDepartment} ) `, function (err, results) {
+                if (err) {
+                console.log(err);
+                };
+                viewRoles();
+            });
+        };
+        newRole();
+    };
+
+    function addEmployee() {
+        async function newEmployee() {
+            const newEmployeePrompt = await inquirer
+            .prompt([
+                {
+                type: "input",
+                name: "empFirstName",
+                message: "What is the first name of the new employee?"
+                },
+                {
+                type: "input",
+                name: "empLastName",
+                message: "What is the last name of the"
+                },
+                {
+                type: "list",
+                name: "roleSel",
+                message: "What role is the employee going to be?",
+                choices: roleArrName
+                },
+    
+            ]);
+        }
+        db.query(``, function (err, results) {
             if (err) {
             console.log(err);
             };
-            viewRoles();
+            console.table(results);
+            loadCLI();
         });
     };
-    newRole();
-};
 
-function addEmployee() {
-    db.query(``, function (err, results) {
-        if (err) {
-        console.log(err);
+    function updateEmployeeRole() {
+
+        async function changeRole() {
+            let changeRolePrompt = await inquirer
+            .prompt([
+                {
+                type: "list",
+                name: "empSel",
+                message: "Which employee are you updating?",
+                choices: empArr
+                },
+                {
+                type: "list",
+                name: "roleSel",
+                message: "Which role are they becoming?",
+                choices: roleArrName
+                }
+            ]);
+            runUpdateQuery(changeRolePrompt);
         };
-        console.table(results);
-        loadCLI();
-    });
-};
 
-function updateEmployeeRole() {
-    const empArr = [];
-    const roleArrName = [];
-    let roleIndex;
+        async function runUpdateQuery(changeRolePrompt) {   
+            const nameStr = changeRolePrompt.empSel
+            const nameArr = nameStr.split(" ");
+            const theIndex = await getIndex(changeRolePrompt);
+
+            db.query(`UPDATE employees
+            SET role_id = ?
+            WHERE employees.first_name = ? AND employees.last_name = ?`, [theIndex, nameArr[0], nameArr[1]], function (err, results) {
+                if (err) {
+                console.log(err);
+                };
+                loadCLI();
+            });
+        };
+
+        changeRole();
+    }; 
+
     function setEmpArr() {
         db.query(`SELECT employees.first_name AS first,
         employees.last_name AS last
@@ -201,7 +258,6 @@ function updateEmployeeRole() {
                 currentEmp += el.first + " " + el.last;
                 empArr.push(currentEmp);
             });
-            setRoleArr();
         });
     };
 
@@ -217,55 +273,43 @@ function updateEmployeeRole() {
                 currentName = el.role;
                 roleArrName.push(currentName);
             });
-            changeRole();
         });
     };
 
-    async function changeRole() {
-        let changeRolePrompt = await inquirer
-        .prompt([
-            {
-            type: "list",
-            name: "empSel",
-            message: "Which employee are you updating?",
-            choices: empArr
-            },
-            {
-            type: "list",
-            name: "roleSel",
-            message: "Which role are they becoming?",
-            choices: roleArrName
-            }
-        ]);
-        getIndex(changeRolePrompt);
-    };
-
-    function runUpdateQuery(changeRolePrompt) {
-        const nameStr = changeRolePrompt.empSel
-        const nameArr = nameStr.split(" ");
-        db.query(`UPDATE employees
-        SET role_id = ?
-        WHERE employees.first_name = ? AND employees.last_name = ?`, [roleIndex, nameArr[0], nameArr[1]], function (err, results) {
-            if (err) {
-            console.log(err);
-            };
-            loadCLI();
-        });
-    };
-
-    function getIndex(changeRolePrompt) {
+    async function getIndex(prompt) {
+        let roleIndex;
         db.query(`SELECT employee_role.id AS roleid
         FROM employee_role
-        WHERE role_title = ?`, changeRolePrompt.roleSel, function (err, results) {
+        WHERE role_title = ?`, prompt.roleSel, function (err, results) {
             if (err) {
             console.log(err);
             };
             roleIndex = results[0].roleid;
-            runUpdateQuery(changeRolePrompt);
+            console.log(roleIndex);
+            return roleIndex;
         });
     };
 
     setEmpArr();
+    setRoleArr();
+    mainMenuPrompt();
 };
 
-module.exports = { loadCLI, viewDepartments, viewRoles, viewEmployees, addDepartment, addRole, addEmployee, updateEmployeeRole };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = { loadCLI }
+
+// module.exports = { loadCLI, viewDepartments, viewRoles, viewEmployees, addDepartment, addRole, addEmployee, updateEmployeeRole };
